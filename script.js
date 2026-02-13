ddd97663362cd6cbc0d8e34102d36e6c5f7d9475
Amin <aminpatni753@gmail.com>
2026-02-13 10:46:35 +0500

    new repo

---
+++ script.js
@@ -0,0 +1,14 @@
+
+
+gsap.to("#main", {
+  backgroundColor: "rgb(255, 255, 255)",
+  scrollTrigger: {
+    trigger: "#main",
+    scroller: "body",
+    // markers: true,
+    start: "top -25%",
+    end: "top -70%",
+    scrub: 2,
+  },
+});
+
